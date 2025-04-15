2f1378ccfc724d082167a36e833b57e14ce4b1b9
LipeMachado <silvalipe765@gmail.com>
2025-04-15 17:22:02 -0300

    Fixed Vectores

--- cpp-modern-beginners/vectores.cpp
+++ cpp-modern-beginners/vectores.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
-  vector<string> lista = {"Shell", "C++", "ReactJs", "NextJs"};
+  vector<string> lista;
+  lista.push_back("Shell");
+  lista.push_back("C++"); 
+  lista.push_back("ReactJs");
+  lista.push_back("NextJs");
   vector<string>::iterator inicio = lista.begin();
 
   cout << "Primeiro do vetor: " << *inicio << endl;
